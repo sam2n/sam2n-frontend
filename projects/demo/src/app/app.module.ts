b4aed04338d390aa325d8ba4d9969e6255e6b758
Stanislav Negreev <stanislav.negreev@ideas.com>
2023-03-23 17:39:09 +0100

    add angular material

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
