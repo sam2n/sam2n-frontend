1e269ab754aa8dc6a3a473c8e83a2a9242bd91a5
Stanislav Negreev <stanislav.negreev@ideas.com>
2023-03-26 18:41:27 +0200

    add login

--- libs/login/src/public-api.ts
+++ libs/login/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of login
  */
 
-export * from './lib/login.service';
-export * from './lib/login.component';
-export * from './lib/login.module';
+export * from "./lib/login.service";
+export * from "./lib/login.component";
+export * from "./lib/login.module";
